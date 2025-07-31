ebd450c0ece8fa0bed84a34d693adf6880d55747
Kushal Shrestha <kushalstha04@gmail.com>
2025-07-31 18:17:41 +0545

    implement complete user-associated cart system and product management

---
+++ backend/src/models/cart/Cart.js
@@ -0,0 +1,47 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../../database/index.js';
+
+const Cart = sequelize.define('Cart', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Users',
+      key: 'id'
+    }
+  },
+  productId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Products',
+      key: 'id'
+    }
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 1,
+    validate: {
+      min: 1
+    }
+  },
+  size: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  color: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+}, {
+  tableName: 'carts',
+  timestamps: true
+});
+
+export { Cart };
