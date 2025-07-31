38216d19410715f775ace4687b252f72d667405f
Kushal Shrestha <kushalstha04@gmail.com>
2025-07-31 18:19:30 +0545

    Add Cart model with user and product relationships

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,1 +1,29 @@
-export * from "./user/User.js";
+export { User } from './user/User.js';
+export { Product } from './Product.js';
+export { Order } from './Order.js';
+export { OrderItem } from './OrderItem.js';
+export { Cart } from './cart/Cart.js';
+
+// Import models for relationships
+import { User } from './user/User.js';
+import { Product } from './Product.js';
+import { Order } from './Order.js';
+import { OrderItem } from './OrderItem.js';
+import { Cart } from './cart/Cart.js';
+
+// Define relationships
+User.hasMany(Order, { foreignKey: 'userId' });
+Order.belongsTo(User, { foreignKey: 'userId' });
+
+Order.hasMany(OrderItem, { foreignKey: 'orderId' });
+OrderItem.belongsTo(Order, { foreignKey: 'orderId' });
+
+Product.hasMany(OrderItem, { foreignKey: 'productId' });
+OrderItem.belongsTo(Product, { foreignKey: 'productId' });
+
+// Cart relationships
+User.hasMany(Cart, { foreignKey: 'userId' });
+Cart.belongsTo(User, { foreignKey: 'userId' });
+
+Product.hasMany(Cart, { foreignKey: 'productId' });
+Cart.belongsTo(Product, { foreignKey: 'productId' });
